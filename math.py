9f8bd896ddbde017574ef98e09cbc76b21e9a4b0
Font Bureau <djr@fontbureau.com>
2015-06-22 10:22:52 -0700

    swap documentation for lerp and norm (oops)

--- math.py
+++ math.py
@@ -2,7 +2,7 @@
 
 def lerp(start, stop, amt):
 	"""
-	Get Interpolated value.
+	Return the interpolation factor (between 0 and 1) of a VALUE between START and STOP.
 	https://processing.org/reference/lerp_.html
 	"""
 	return float(amt-start) / float(stop-start)
@@ -10,7 +10,7 @@ def lerp(start, stop, amt):
 def norm(value, start, stop):
 	"""
 	Interpolate.
-	Return the interpolation factor (between 0 and 1) of a VALUE between START and STOP.
+	Get Interpolated value, between zero and one.
 	See also: https://processing.org/reference/norm_.html
 	"""
 	return start + (stop-start) * value
