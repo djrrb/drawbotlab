431f17fe45f2876c4f78f4c3a88e9c4afc32f303
David Jonathan Ross <david@djr.com>
2020-03-05 13:38:32 -0500

    use vars instead of tuples (thanks nina!)

--- glyph.py
+++ glyph.py
@@ -7,14 +7,15 @@ class DrawBotPen(BasePen):
     A pen that draws a glyph into a drawbot.
     I don't think this can deal with components or anything so be careful.
     """
-    def _moveTo(self, (x, y)):
-        moveTo((x, y))
-    def _lineTo(self, (x, y)):
-        lineTo((x, y))
-    def _curveToOne(self, (x1, y1), (x2, y2), (x3, y3)):
-        curveTo((x1, y1), (x2, y2), (x3, y3))
+    def _moveTo(self, coordinate):
+        moveTo(coordinate)
+    def _lineTo(self, coordinate):
+        lineTo(coordinate)
+    def _curveToOne(self, coordinate1, coordinate2, coordinate3):
+        curveTo(coordinate1, coordinate2, coordinate3)
     def closePath(self):
         closePath()
+
 
 def drawGlyph(glyph):
     """
